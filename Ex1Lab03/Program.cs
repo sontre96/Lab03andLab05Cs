98cb3f6f3d34760dacb2cce9b0a190ce50a6ede9
sontre96 <sontre96@gmail.com>
2022-06-08 14:56:50 +0700

    push file lab03 c#

---
+++ Ex1Lab03/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Ex1Lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar;
+            System.Console.WriteLine("Creating a Car object and assigning"
+                + "its memory location to myCar");
+
+            myCar = new Car();
+
+            myCar.Make = "Toyota";
+            myCar.Model = "MR2";
+            myCar.Color = "Black";
+            myCar.yearBuilt = 1995;
+
+            // display
+            System.Console.WriteLine("My Car details: ");
+            System.Console.WriteLine("myCar.Make = " + myCar.Make);
+            System.Console.WriteLine("myCar.Model = "+ myCar.Model);
+            System.Console.WriteLine("myCar.Color = " + myCar.Color);
+            System.Console.WriteLine("myCar.yearBuilt = " + myCar.yearBuilt);
+
+            myCar.Start();
+            myCar.Stop();
+
+            // Create another Car object
+            System.Console.WriteLine("Creating another Car object and"
+                + "assigning its memory location to redPorsche");
+            Car redPorsche = new Car();
+            redPorsche.Make = "Porsche";
+            redPorsche.Model = "Boxster";
+            redPorsche.Color = "red";
+            redPorsche.yearBuilt = 2000;
+            System.Console.WriteLine("redPorsche is a " + redPorsche.Model);
+            System.Console.WriteLine("Assigning redPorscher to myCar");
+            myCar = redPorsche;
+            System.Console.WriteLine("My Car details: ");
+            System.Console.WriteLine("myCar.Make = " + myCar.Make);
+            System.Console.WriteLine("myCar.Model = " + myCar.Model);
+            System.Console.WriteLine("myCar.Color = " + myCar.Color);
+            System.Console.WriteLine("myCar.yearBuilt = " + myCar.yearBuilt);
+            // assign null myCar (myCar will no longer reference an object)
+            myCar = null;
+            Console.ReadLine();
+        }
+    }
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int yearBuilt { get; set; }
+
+        public void Start()
+        {
+            System.Console.WriteLine(Model + "started");
+        }
+
+        public void Stop()
+        {
+            System.Console.WriteLine(Model + "Stopped");
+
+        }
+    }
+}
+
---
+++ Ex2Lab03/Window.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2Lab03
+{
+    class Window
+    {
+        protected int top;
+        protected int left;
+
+        public Window(int top, int left)
+        {
+            this.top = top;
+            this.left = left;
+        }
+
+        // simulates drawing the window
+        public virtual void DrawWindow()
+        {
+            Console.WriteLine("Window: drawing Window at {0}, {1}", top, left);
+        }
+    }
+
+    // Create class ListBox
+    class ListBox : Window
+    {
+        private string listBoxContents { get; set; }
+
+        public ListBox(int top, int left, string contents) : base(top, left) // call base constructor
+        {
+            listBoxContents = contents;
+        }
+
+        public override void DrawWindow()
+        {
+            base.DrawWindow();//invoke the base method
+            Console.WriteLine("Writing string to the listBox: {0}", listBoxContents);
+
+        }
+    }
+
+    class Button : Window
+    {
+        public Button(int top, int left) : base(top, left)
+        {
+        }
+
+        public override void DrawWindow()
+        {
+            Console.WriteLine("Drawing a button at {0} {1}\n", top, left);
+        }
+    }
+}
